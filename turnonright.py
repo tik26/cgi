46a3487840a89c1f7aa33fdfe67e679007de23ee
tik26 <53051652+tik26@users.noreply.github.com>
2021-02-27 15:15:27 +0900

    Add files via upload

---
+++ turnonright.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+
+LEFT_TIRE = 17
+RIGHT_TIRE = 18
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LEFT_TIRE, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(RIGHT_TIRE, GPIO.OUT, initial=GPIO.LOW)
+
+def main():
+    setup()
+    GPIO.output(LEFT_TIRE, GPIO.HIGH)
+    GPIO.output(RIGHT_TIRE, GPIO.LOW)
+
+def end():
+    GPIO.output(LEFT_TIRE, GPIO.LOW)
+    GPIO.output(RIGHT_TIRE, GPIO.LOW)
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        end()
+
+        
